472f380f3cd50353fe803b64a82c72657bcbb97f
Jaccob <783739330@qq.com>
2025-04-14 10:46:48 +0800

    Add SquareLine Project files

---
+++ SquareLine UI Project/HelloBalls_ui_events.py
@@ -0,0 +1,20 @@
+
+def can_task(event_struct):
+    return
+
+
+def lvgl_can_task(event_struct):
+    return
+
+
+def start_twai_receive(event_struct):
+    return
+
+
+def lvgl_start_twai_receive(event_struct):
+    return
+
+
+def lvgl_stop_twai_receive(event_struct):
+    return
+
